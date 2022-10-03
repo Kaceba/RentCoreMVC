0783ef6b8400dfde2e09e9f436955d7a542f5dd0
Roberto Ninno <RobertoNinno23@outlook.it>
2022-10-03 17:16:18 +0200

    changes

---
+++ Models/Services/Application/TipiSpeseService.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+public class TipiSpeseService : ITipiSpeseService
+{
+    public List<TipiSpeseViewModel> GetTipiSpese()
+    {
+        var TipiSpeseList = new List<TipiSpeseViewModel>();
+
+
+
+
+
+
+
+        return TipiSpeseList;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<ITipiSpeseService, TipiSpeseService>();
 
 var app = builder.Build();
 
