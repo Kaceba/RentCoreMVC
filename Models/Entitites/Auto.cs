a17132c315396aed7b788280e64a21488a0fcbf8
Kaceba <RobertoNinno23@outlook.it>
2022-10-07 17:58:42 +0200

    added entity framework structure

---
+++ Models/Entitites/Auto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentCore.Models.Entitites
+{
+    public partial class Auto
+    {
+        public long IdAuto { get; set; }
+        public long? IdModello { get; set; }
+        public string? Targa { get; set; }
+        public byte[]? Costo { get; set; }
+        public byte[]? DataAcquisto { get; set; }
+    }
+}
---
+++ Models/Entitites/Marche.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentCore.Models.Entitites
+{
+    public partial class Marche
+    {
+        public long IdMarca { get; set; }
+        public string DescrizioneMarca { get; set; } = null!;
+    }
+}
